21cd83bac7d6d5fd485393ec9ac49470fedc2418
antonlin1 <anton.lin1@gmail.com>
2015-05-21 12:10:24 +0200

    fixed header stuff

---
+++ spacedefence/header.js
@@ -0,0 +1,1 @@
+document.write('<div class="header"><div class="navigationMenu"><ul><li><a href="./index.html">Home</a></li><li><a href="./manual.html">Manual</a></li><li><a href="./kontakt.html">Contact</a></li><li><a href="./download.html">Download</a></li></ul></div></div>');
